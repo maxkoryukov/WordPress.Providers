285c57c3d4f06682b56ea7c7243ee5e4fcfbcf8e
maxkoryukov <maxkoryukov@yandex.ru>
2015-08-27 04:17:34 +0500

    wp api stub added

---
+++ src/WordPress.Providers/WpApi/WordpressRoleProvider.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Web.Security;
+
+namespace WordPress.Providers.WpApi
+{
+	public class WordpressRoleProvider : RoleProvider
+	{
+		public override string ApplicationName{get;set;}
+
+		public override bool IsUserInRole(string username, string rolename)
+		{
+			if (null != rolename)
+				rolename = rolename.ToLower();
+			
+			if ("administrator" == rolename)
+				return true;
+			
+			return false;
+		}
+
+		public override string[] GetRolesForUser(string username)
+		{
+			return new string[] {"Administrator"};
+		}
+
+		#region Not Implemented
+
+		public override void AddUsersToRoles(string[] usernames, string[] rolenames)
+		{
+			throw new NotImplementedException();
+		}
+			
+		public override void CreateRole(string rolename)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override bool DeleteRole(string rolename, bool throwOnPopulatedRole)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override string[] GetAllRoles()
+		{
+			throw new NotImplementedException();
+		}
+			
+		public override string[] GetUsersInRole(string rolename)
+		{
+			throw new NotImplementedException();
+		}
+			
+		public override void RemoveUsersFromRoles(string[] usernames, string[] rolenames)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override bool RoleExists(string rolename)
+		{
+			throw new NotImplementedException();
+		}
+
+
+		#endregion
+	}
+}
+
