bab46805e695fc4b239c93874f71142217ed9198
maxkoryukov <maxkoryukov@yandex.ru>
2015-08-27 04:01:33 +0500

    move files

--- src/WordPress.Providers/Properties/AssemblyInfo.cs
+++ src/WordPress.Providers/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("WordPress.Providers.MySql")]
+[assembly: AssemblyTitle("WordPress.Providers")]
 [assembly: AssemblyDescription(".NET Providers over the MySql Of WordPress's site")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Max Koryukov")]
-[assembly: AssemblyProduct("WordPress.Providers.MySql")]
+[assembly: AssemblyProduct("WordPress.Providers")]
 [assembly: AssemblyCopyright("© 2015 Max Koryukov")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyVersion("0.0.1.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
